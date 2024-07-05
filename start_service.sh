239789f88fa2299c74c0270453f6e607f62b7b87
zhongwenhao-dm <wenhao.zhong@deepmirror.com>
2024-07-05 09:52:49 +0800

    add sudo

--- start_service.sh
+++ start_service.sh
@@ -3,7 +3,7 @@
 # 创建并启动容器
 CONTAINER_NAME=test1
 echo "Target container's name is '$CONTAINER_NAME'!"
-if docker inspect "$CONTAINER_NAME" > /dev/null 2>&1; then
+if sudo docker inspect "$CONTAINER_NAME" > /dev/null 2>&1; then
     echo "Container '$CONTAINER_NAME' exists!"
 else
     echo "Container '$CONTAINER_NAME' does not exist, create it!"
@@ -13,23 +13,23 @@ fi
 # 捕获ctrl+c，终止数据录制
 cleanup() {
     echo "Caught interrupt signal! Terminating process..."
-    PID_POSE=$(docker exec $CONTAINER_NAME pgrep -f pose_server_component_main)
-    PID_IMU=$(docker exec $CONTAINER_NAME pgrep -f imu_component_main)
-    PID_GPS=$(docker exec $CONTAINER_NAME pgrep -f gps_component_main)
-    docker exec $CONTAINER_NAME kill -9 $PID_POSE $PID_GPS $PID_IMU
+    PID_POSE=$(sudo docker exec $CONTAINER_NAME pgrep -f pose_server_component_main)
+    PID_IMU=$(sudo docker exec $CONTAINER_NAME pgrep -f imu_component_main)
+    PID_GPS=$(sudo docker exec $CONTAINER_NAME pgrep -f gps_component_main)
+    sudo docker exec $CONTAINER_NAME kill -9 $PID_POSE $PID_GPS $PID_IMU
     exit 0
 }
 trap cleanup INT
 
 # 启动imu_component和gps_component
 echo "Start imu_component and gps_component!"
-gnome-terminal -- bash -c "docker exec $CONTAINER_NAME bash -c "./components/drivers/imu_component_main"; exec bash"
-gnome-terminal -- bash -c "docker exec $CONTAINER_NAME bash -c "./components/drivers/gps_component_main"; exec bash"
-sleep 6
+gnome-terminal -- bash -c "sudo docker exec $CONTAINER_NAME bash -c "./components/drivers/imu_component_main"; exec bash"
+gnome-terminal -- bash -c "sudo docker exec $CONTAINER_NAME bash -c "./components/drivers/gps_component_main"; exec bash"
+sleep 10
 
 # 启动pose_server_component
 CONFIG_FILE=msp20_pose_server_rk3566.proto.txt
-docker cp ./config/$CONFIG_FILE $CONTAINER_NAME:/work/launch/xavier_local/$CONFIG_FILE
-docker exec $CONTAINER_NAME bash -c "./components/map/pose_server_component_main --config_file /work/launch/xavier_local/$CONFIG_FILE"
+sudo docker cp ./config/$CONFIG_FILE $CONTAINER_NAME:/work/launch/xavier_local/$CONFIG_FILE
+sudo docker exec $CONTAINER_NAME bash -c "./components/map/pose_server_component_main --config_file /work/launch/xavier_local/$CONFIG_FILE"
 
 sleep 100
