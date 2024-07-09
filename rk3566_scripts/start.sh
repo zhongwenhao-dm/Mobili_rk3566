6e5bdf938994a6a6f3bbbe3683a7538926df0a01
zhongwenhao-dm <wenhao.zhong@deepmirror.com>
2024-07-09 17:31:52 +0800

    fix

--- rk3566_scripts/start.sh
+++ rk3566_scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker-compose -f /work/Mobili_rk3566/docker_compose/docker-compose-arm.yml up
+sudo docker-compose -f ~/work/Mobili_rk3566/docker_compose/docker-compose-arm.yml up
