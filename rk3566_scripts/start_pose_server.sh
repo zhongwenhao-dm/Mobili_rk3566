624281ae1fd9c73160fc17f270a9451846112154
zhongwenhao-dm <wenhao.zhong@deepmirror.com>
2024-07-19 16:57:54 +0800

    add record for pose_server

--- rk3566_scripts/start_pose_server.sh
+++ rk3566_scripts/start_pose_server.sh
@@ -5,9 +5,17 @@
 PARENT_DIR=/home/cat/Data
 YML_FILE=~/work/Mobili_rk3566/docker_compose/docker-compose-pose_server-arm.yml
 
+TEMP_DIR=$PARENT_DIR/../temp
+mkdir $TEMP_DIR
 
-# 启动compose
-sudo docker-compose -f $YML_FILE up
+# 给保存的数据文件夹的后缀名
+OUTPUT_DIR_NAME=$1
 
-echo "docker-compose-pose_server启动！"
+# 启动compose， 输出到日志文件
+sudo docker-compose -f $YML_FILE up > $TEMP_DIR/docker-compose.log 2>&1 &
+
+COMPOSE_PID=$!
+echo $COMPOSE_PID > $TEMP_DIR/docker-compose.pid
+echo $OUTPUT_DIR_NAME > $TEMP_DIR/output-dir.name
+echo "docker-compose启动！"
 
--- rk3566_scripts/stop_pose_server.sh
+++ rk3566_scripts/stop_pose_server.sh
@@ -5,3 +5,48 @@
 PARENT_DIR=/home/cat/Data
 YML_FILE=~/work/Mobili_rk3566/docker_compose/docker-compose-pose_server-arm.yml
 
+TEMP_DIR=$PARENT_DIR/../temp
+
+echo "Stop data recording!"
+
+# 读取进程id，杀死进程
+COMPOSE_PID=$(cat $TEMP_DIR/docker-compose.pid)
+sudo kill $COMPOSE_PID
+
+sudo docker-compose -f $YML_FILE down
+
+# 查找最近新增的子文件夹
+OUTPUT_DIR_NAME=$(cat $TEMP_DIR/output-dir.name)
+LATEST_DIR=$(find "$PARENT_DIR" -mindepth 1 -maxdepth 1 -type d -printf "%T@ %p\n" | sort -n | tail -1 | cut -d' ' -f2-)
+# 检查是否找到子文件夹
+if [ -z "$LATEST_DIR" ]; then
+    echo "没有找到新增的子文件夹。"
+    exit 0
+fi
+# 移动log文件到最新文件夹中
+mv "$TEMP_DIR/docker-compose.log" "$LATEST_DIR/docker-compose.log"
+
+# 找到log中的GPS_TIME
+# I20240717 14:18:39.930855 1346944 serial_port_gps.cc:133] GPS time = 20240705203456
+LOG_FILE=$LATEST_DIR/docker-compose.log
+GPS_TIME=$(grep -m 1 "GPS time =" "$LOG_FILE" | awk -F'=' '{print $2}' | awk '{$1=$1;print}')
+# 如果找到了GPS_TIME，将其作为文件夹的名称
+if [ -n "${GPS_TIME}" ]; then
+    mv "$LATEST_DIR" "$PARENT_DIR/$GPS_TIME"
+    LATEST_DIR="$PARENT_DIR/$GPS_TIME"
+fi
+
+# 如果存在输入，重命名最新的文件夹
+if [ -n  "${OUTPUT_DIR_NAME}" ]; then
+    BASE_NAME=$(basename "$LATEST_DIR")
+    NEW_NAME="${BASE_NAME}_${OUTPUT_DIR_NAME}"
+    mv "$LATEST_DIR" "$PARENT_DIR/$NEW_NAME"
+    echo $PARENT_DIR/$NEW_NAME
+else
+    echo $LATEST_DIR
+fi
+
+rm -rf $TEMP_DIR
+echo "delete $TEMP_DIR"
+
+exit 0
